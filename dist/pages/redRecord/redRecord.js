a01b2f92cd667f02805cf2b664150ac663df0ceb
haoya.xu <haoya.xu@datuhongan.com>
2018-10-15 16:17:01 +0800

    init commit

---
+++ dist/pages/redRecord/redRecord.js
@@ -0,0 +1,13 @@
+"use strict";
+
+bui.ready(function () {
+    //点击跳转
+    $(".recordList li").on("click", function (e) {
+        bui.load({ url: "/pages/redRecordDetail/redRecordDetail.html" });
+    });
+
+    //返回
+    $(".icon-back").on("click", function (e) {
+        history.back();
+    });
+});
